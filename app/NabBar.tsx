7602141538482f33e13d7ad639150c4417a881bf
Jameel Ahmed <45817812+jameel-fullstackdeveloper@users.noreply.github.com>
2023-10-11 23:21:18 +0500

    Navbar Created

---
+++ app/NabBar.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'next/link'
+import { AiFillBug } from 'react-icons/ai'
+
+const NabBar = () => {
+    const links = [
+        {lable: 'Dashboard', href: '/'},
+        {lable: 'Issues', href: '/issues'},
+    ]
+  return (
+    <nav className='flex space-x-6 border-b px-5 mb-5 h-14 items-center' >
+        <Link href="/"><AiFillBug /></Link>
+        <ul className='flex space-x-6'>
+            {links.map(link=>
+                <Link key={link.href}  href={link.href} className='text-zinc-500 hover:text-zinc-800 transition-colors'> {link.lable }</Link>
+            )}
+
+        </ul>
+    </nav>
+  )
+}
+
+export default NabBar
